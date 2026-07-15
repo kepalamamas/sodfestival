7d724476956710909d631ee5cdea51b914434b00
Indrakusuma <kindrakusuma@deloitte.com>
2026-07-15 17:28:25 +0700

    update asset

--- vite.config.js
+++ vite.config.js
@@ -76,7 +76,8 @@ export default defineConfig(({ mode }) => {
           main: resolve(__dirname, 'index.html'),
           merch: resolve(__dirname, 'merch.html'),
           track: resolve(__dirname, 'track.html'),
-          dresscode: resolve(__dirname, 'dresscode.html')
+          dresscode: resolve(__dirname, 'dresscode.html'),
+          volunteer: resolve(__dirname, 'volunteer.html')
         }
       },
       // Optimize assets
